5f990fb2f59baf41958ee643dda41e7e58471f5a
Andrey Cherepanov <cherepanov2112@gmail.com>
2022-01-19 00:19:33 +0700

    Fixed style

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,28 +1,22 @@
 
 <nav x-data class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand" href="#">GuessNumber</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('home') }}">Home</a>
-            </li>
             <li class="nav-item dropdown">
                 @auth
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">{{ Auth::user()->name }}</a>
                 @else
-                    <a href="{{ route('login') }}" class="nav-link">Log in</a>
+                    <a href="{{ route('login') }}" class="nav-link">Авторизация</a>
 
                     @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="nav-link">Register</a>
+                        <a href="{{ route('register') }}" class="nav-link">Регистрация</a>
                     @endif
                 @endauth
                 <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <a class="dropdown-item" href="{{ route('home') }}">Home</a>
                     <div class="dropdown-divider"></div>
 
                     <form method="POST" action="{{ route('logout') }}">
