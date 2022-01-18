4ac041e3ba649d37d70d15128faec14ddaa1b8c0
Andrey Cherepanov <cherepanov2112@gmail.com>
2022-01-18 23:48:58 +0700

    add home route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <div class="home-page">
+        <div class="block-game">
+            <div class="h1">
+                <h1>Угадай число</h1>
+            </div>
+            <div class="find">
+                <button class="btn btn-lg btn-primary btn-block btn-find" type="submit">Найти игру</button>
+            </div>
+            <div class="list">
+                <button class="btn btn-lg btn-primary btn-block btn-list" type="submit">Журнал матчей</button>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index'])
+    ->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
