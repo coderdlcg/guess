25efb7df202d72c9edf460b4168357bf6d237bed
Andrey Cherepanov <cherepanov2112@gmail.com>
2022-01-28 23:12:46 +0700

    fixed templates

--- resources/js/components/GameComponent.vue
+++ resources/js/components/GameComponent.vue
@@ -1,47 +1,79 @@
 <template>
-    <div class="gameround p-3">
-        <div class="table-round">
-            <table class="table">
-                <thead>
-                <tr>
-                    <th scope="col">Ход</th>
-                    <th scope="col">Компьютер</th>
-                    <th scope="col">Том</th>
-                    <th scope="col">Bob</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>12</td>
-                    <td class="success">10</td>
-                    <td class="failed">16</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>16</td>
-                    <td class="normal">12</td>
-                    <td class="normal">12</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-
-        <div class="messages" v-if="messages.length">
-            <div class="message" v-for="message in messages">
-                <span class="d-inline-block">{{ message }}</span>
+    <div class="game-page row">
+        <div class="left-player in_active col-3">
+            <div class="player-name">
+                <h2>{{activeUsers[0]}}</h2>
             </div>
-        </div>
-
-        <div class="footer">
-            <div class="row">
-                <div class="col">
-                    <input class="form-control input_number" type="text" placeholder="Введите число" v-model="textMessage">
+            <div class="number">
+                13
+            </div>
+            <div class="footer">
+                <div class="info">
+                    <p>Ваш ход</p>
+                    <a href="#" class="exit_game" data-toggle="modal" data-target="#staticBackdrop">Покинуть игру</a>
                 </div>
             </div>
-            <div class="row">
-                <div class="col mt20">
-                    <button class="btn btn-lg btn-primary btn-block btn-send_number" @click="sendMessage()">Угадать</button>
+        </div>
+        <div class="game-info col-6">
+            <div class="gameround p-3">
+                <div class="table-round">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">Ход</th>
+                            <th scope="col">Компьютер</th>
+                            <th scope="col">Том</th>
+                            <th scope="col">Bob</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>12</td>
+                            <td class="success">10</td>
+                            <td class="failed">16</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>16</td>
+                            <td class="normal">12</td>
+                            <td class="normal">12</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="messages" v-if="messages.length">
+                    <div class="message" v-for="message in messages">
+                        <span class="d-inline-block">{{ message }}</span>
+                    </div>
+                </div>
+
+                <div class="footer">
+                    <div class="row">
+                        <div class="col">
+                            <span v-if="isActive">{{isActive.name}} набирает сообщение...</span>
+                            <input class="form-control input_number" type="text" placeholder="Введите число" v-model="textMessage" @keyup.enter="sendMessage" @keydown="actionUser">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col mt20">
+                            <button class="btn btn-lg btn-primary btn-block btn-send_number" @click="sendMessage">Угадать</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="right-player col-3">
+            <div class="player-name" >
+                <h2>{{activeUsers[1]}}</h2>
+            </div>
+            <div class="number">
+                13
+            </div>
+            <div class="footer">
+                <div class="info">
+                    Ход противника
                 </div>
             </div>
         </div>
@@ -50,29 +82,55 @@
 
 <script>
 export default {
+    props: ['game', 'user'],
     data() {
         return {
-            textMessage: '',
             messages: [],
+            textMessage: '',
+            isActive: false,
+            typingTimer: false,
+            activeUsers: []
         }
     },
-    created() {
-        this.addMessage('You joined the game.');
-        Echo.channel('game')
-            .listen('MessageSend', (e) => {
-                this.addMessage(e.message);
+    computed: {
+        channel() {
+            return window.Echo.join('game.' + this.game.id);
+        }
+    },
+    mounted() {
+        this.channel
+            .here((users) => {
+                this.activeUsers = users;
+            })
+            .joining((user) => {
+                this.activeUsers.push(user);
+            })
+            .leaving((user) => {
+                this.activeUsers.splice(this.activeUsers.indexOf(user), 1);
+            })
+            .listen('MessageSend', ({message}) => {
+                this.messages.push(message.body);
+                this.isActive = false;
+            })
+            .listenForWhisper('typing', (e) => {
+                this.isActive = e;
+                if(this.typingTimer) clearTimeout(this.typingTimer);
+                this.typingTimer = setTimeout(() => {
+                    this.isActive = false;
+                }, 2000);
             });
     },
     methods: {
-        addMessage(message) {
-            // let date= new Date();
-            // let timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-            // this.messages.push(timestamp + ' ' + message);
-            this.messages.push(message);
-        },
         sendMessage() {
-            axios.post('/api/message', {message: this.textMessage});
+            axios.post('/messages', { body: this.textMessage, game_id: this.game.id });
+            this.messages.push(this.textMessage);
             this.textMessage = '';
+        },
+        actionUser() {
+            this.channel
+                .whisper('typing', {
+                    name: this.user.name
+                });
         }
     }
 }
--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -1,48 +1,17 @@
 <x-app-layout>
-    <div class="game-page row">
-        <div class="left-player in_active col-3">
-            <div class="player-name">
-                <h2>Tom</h2>
-            </div>
-            <div class="number">
-                13
-            </div>
-            <div class="footer">
-                <div class="info">
-                    <p>Ваш ход</p>
-                    <a href="#" class="exit_game" data-toggle="modal" data-target="#staticBackdrop">Покинуть игру</a>
-                </div>
-            </div>
-        </div>
-        <div class="game-info col-6">
-            <div id="app">
-                <game-component></game-component>
-            </div>
-        </div>
-        <div class="right-player col-3">
-            <div class="player-name">
-                <h2>Bob</h2>
-            </div>
-            <div class="number">
-                13
-            </div>
-            <div class="footer">
-                <div class="info">
-                    Ход противника
-                </div>
-            </div>
-        </div>
+    <div id="app">
+        <game-component :game="{{$game}}" :user="{{Auth::user()}}"></game-component>
+    </div>
 
-        <!-- Modal -->
-        <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-body">
-                        <div class="text winner">Победа</div>
-                    </div>
-                    <div class="modal-footer">
-                        <a href="{{ route('home') }}" class="btn btn-secondary btn-close">В главное меню</a>
-                    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="text winner">Победа</div>
+                </div>
+                <div class="modal-footer">
+                    <a href="{{ route('home') }}" class="btn btn-secondary btn-close">В главное меню</a>
                 </div>
             </div>
         </div>
