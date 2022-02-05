2b585a1772b303e3b239e22629124d3f212f4264
Andrey Cherepanov <cherepanov2112@gmail.com>
2022-02-05 20:23:13 +0700

    fixed including bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,16 @@
+
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
