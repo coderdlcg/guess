9d19b20b8cd1cfa858e7e6f81603193566069dc8
Andrey Cherepanov <cherepanov2112@gmail.com>
2022-02-06 18:47:12 +0700

    fixed exit from app

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,4 @@
 <x-app-layout>
-    <header>
-        @include('layouts.navigation')
-    </header>
     <div class="home-page">
         <div class="block-game">
             <div class="h1">
@@ -11,6 +8,19 @@
             <div class="list">
                 <a href="{{ route('history') }}" class="btn btn-lg btn-primary btn-block btn-list" type="submit">Журнал матчей</a>
             </div>
+
+            <div class="exit">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a class="btn btn-lg btn-primary btn-block btn-list"
+                       href="{{ route('logout') }}"
+                       onclick="event.preventDefault(); this.closest('form').submit();"
+                    >
+                        {{ __('Log Out') }}
+                    </a>
+                </form>
+            </div>
+
         </div>
     </div>
 </x-app-layout>
