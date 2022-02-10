900575f93297f2ca0e55c3a46f54a7061184731c
Andrey Cherepanov <cherepanov2112@gmail.com>
2022-02-10 23:22:31 +0700

    Added new events.

---
+++ app/Events/FindGame.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class FindGame implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+
+    public function __construct($message)
+    {
+        $this->message = $message;
+
+        Log::channel('daily')->log('info', 'Event FindGame', [$this->message]);
+    }
+
+    public function broadcastOn(): Channel
+    {
+        return new Channel('find');
+    }
+}
